576eae5a19ec17bb090c8a3bf839a4a88c7ef730
Mark Simpson <mark.simpson78@hotmail.co.uk>
2013-12-29 23:39:21 +0000

    Commit Info

--- scripts/grungemofo-start_up.js
+++ scripts/grungemofo-start_up.js
@@ -1,8 +1,23 @@
 // JavaScript Document
 
+function on_before() {
+	console.log('before');
+	jQuery('.cycle-title').fadeOut(500);
+}
+
+function on_after() {
+	console.log('after');
+	jQuery('.cycle-title').fadeIn(500);
+}
+
 // jcycle instantiate
 function start_cycle() {
-	jQuery('.jcycle-item').cycle();
+	jQuery('.jcycle-item').cycle({ 
+    fx:    'fade', 
+    speed:  2500,
+	before:	on_before,
+	after: on_after
+ });
 }
 
 
